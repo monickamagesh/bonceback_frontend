42c2af1b829f6301ab5bfa35c23f0f72574111b4
monickamagesh <monickamagesh@gmail.com>
2025-01-03 05:24:02 +0530

    get base url for production

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,7 @@
+//export const getBaseUrl = () => {
+//    return "//localhost:5000";
+//}
+
 export const getBaseUrl = () => {
-    return "//localhost:5000";
+    return "https://bounceback-backend.vercel.app";
 }
